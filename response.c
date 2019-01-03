4a5a88988c846f357835ba141de38f4b6e4a3c2c
Bogdan Bolbot <Bolbot2018@outlook.com>
2019-01-03 07:14:29 +0200

    provided necessary headers in response.c

--- response.c
+++ response.c
@@ -1,8 +1,12 @@
+#include <time.h>
 #include <regex.h>
 #include <stdio.h>
+#include <errno.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 #include "response.h"
 #include "defines.h"
